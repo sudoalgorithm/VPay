a76d02b4424e4450476ccb0c102fe47972d6b48d
Kunal Malhotra <kunalmalhotra@Kunals-MacBook-Pro.local>
2016-11-26 03:12:31 +0400

    Facebook login

--- app/src/main/java/sudoalgorithm/visahackathon/MainActivity.java
+++ app/src/main/java/sudoalgorithm/visahackathon/MainActivity.java
@@ -1,13 +1,59 @@
 package sudoalgorithm.visahackathon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+            }
+
+            @Override
+            public void onCancel() {
+                Snackbar.make(mRelativeLayout, "Transaction Canceled",Snackbar.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Snackbar.make(mRelativeLayout, "Network Error, Please Try Again",Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
