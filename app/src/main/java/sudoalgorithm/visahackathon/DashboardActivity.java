3e8e4b4df6a613055852fe599a17d3f08f4101a6
Kunal Malhotra <kunalmalhotra@Kunals-MacBook-Pro.local>
2016-11-26 03:20:27 +0400

    Toolbar Commit

--- app/src/main/java/sudoalgorithm/visahackathon/DashboardActivity.java
+++ app/src/main/java/sudoalgorithm/visahackathon/DashboardActivity.java
@@ -1,13 +1,24 @@
 package sudoalgorithm.visahackathon;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private ActionBar mActionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mActionBar = getSupportActionBar();
+        if (mActionBar != null){
+            mActionBar.setDisplayShowTitleEnabled(false );
+        }
     }
 }
